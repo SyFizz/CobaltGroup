6330fc868a51518a11c536ac902ddf64e78bdb64
Grégory BERTINI <gbertini.lpo@gmail.com>
2023-08-29 08:42:16 +0200

    Added session alerts to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,16 @@
                     </div>
                 </header>
             @endif
+            @if(session('success'))
+                <div class="mx-6 mt-3 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">{{ session('success') }}</strong>
+                </div>
+            @endif
+            @if(session('error'))
+                <div class="mx-6 mt-3 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">{{ session('error') }}</strong>
+                </div>
+            @endif
 
             <!-- Page Content -->
             <main>
