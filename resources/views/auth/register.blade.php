1add2727d36c0c0934b66407a22cbc5bbe37b42c
Grégory BERTINI <gbertini.lpo@gmail.com>
2023-08-29 11:14:11 +0200

    Changed favicon and added a button on disabled register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,5 +7,10 @@
             {{__("La création de compte est désactivée pour le moment.")}} <br>
             {{__("Contactez un administrateur afin d'obtenir un compte.")}}
         </div>
+        <div class="text-center">
+            <x-primary-button class="mt-4 w-full text-center align-middle justify-center " href="{{ route('login') }}">
+                {{ __('Se connecter') }}
+            </x-primary-button>
+        </div>
     </form>
 </x-guest-layout>
