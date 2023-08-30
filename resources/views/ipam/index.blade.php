2a5001ec91cd9dcc12d18d9f2ba0b829ee61040e
Grégory BERTINI <gbertini.lpo@gmail.com>
2023-08-30 12:11:22 +0200

    Added some test design to ipam index

--- resources/views/ipam/index.blade.php
+++ resources/views/ipam/index.blade.php
@@ -6,14 +6,29 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-3 text-gray-900">
-                    <div class="w-1/4 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="bg-white shadow-sm sm:rounded-lg">
+                <div class="p-3 text-gray-900 flex space-x-10 h-full">
+                    <div class="w-1/4 h-auto p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nombre total de blocs</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Blocs publics : {{\App\Models\AddressBlock::all()->where("type", "public")->count()}}
+                        <br>Blocs privés : {{\App\Models\Address::all()->where("type", "private")->count()}}
+                        <br><br>Total : </p>
+                    </div>
+                    <div class="w-1/3 h-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                         </a>
                         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     </div>
+                    <div class="w-1/3 h-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                        </a>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </div>
+
                 </div>
             </div>
         </div>
